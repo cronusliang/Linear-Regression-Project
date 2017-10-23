6f3e881c44048c6d6d55b9f61c085a067651b306
cronus <cronusliang@gmail.com>
2017-10-24 00:01:11 +0800

    add part of work

---
+++ linear_regression_project.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+# 本项目要求矩阵统一使用二维列表表示，如下：
+A = [[1,2,3],
+     [2,3,3],
+     [1,2,5]]
+
+B = [[1,2,3,5],
+     [2,3,3,5],
+     [1,2,5,1]]
+
+#TODO 创建一个 4*4 单位矩阵
+def identity_matrix(n):
+    I = [0] * n
+    for i in range(0, n):
+        I[i] = [0]*n
+        I[i][i] = 1
+    return I
+print identity_matrix(4)
+
